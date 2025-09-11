0ccb538c09a942b9f42ddd462aa662b554a8b8b9
Stavros Kois <s.kois@outlook.com>
2025-09-11 12:06:46 +0300

    init generator

---
+++ pkg/ws/generate/generate.go
@@ -0,0 +1,40 @@
+package generate
+
+import "reflect"
+
+type handlerInfo struct {
+	reqType  any
+	respType any
+	docs     HandlerDocs
+}
+
+type HandlerDocs struct {
+}
+
+type eventType struct {
+	respType any
+	docs     EventDocs
+}
+
+type EventDocs struct {
+}
+
+type Generator struct {
+	typeCache    map[reflect.Type]string
+	eventTypes   map[string]eventType
+	handlerTypes map[string]handlerInfo
+}
+
+func NewGenerator() *Generator {
+	return &Generator{
+		typeCache: make(map[reflect.Type]string),
+	}
+}
+
+func (g *Generator) AddEventType(name string, resp any, docs EventDocs) {
+	g.eventTypes[name] = eventType{respType: resp, docs: docs}
+}
+
+func (g *Generator) AddHandlerType(name string, req any, resp any, docs HandlerDocs) {
+	g.handlerTypes[name] = handlerInfo{reqType: req, respType: resp, docs: docs}
+}
