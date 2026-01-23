c72fcf440d3c7219998bc97f4d3d898134abf2ea
Stavros Kois <s.kois@outlook.com>
2026-01-24 01:46:29 +0200

    feat: Enhance main server setup with graceful shutdown and improved logging

--- backend/cmd/rest/main.go
+++ backend/cmd/rest/main.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"ws-json-rpc/backend/internal/app"
 	"ws-json-rpc/backend/internal/httpapi"
 	"ws-json-rpc/backend/pkg/router"
+	"ws-json-rpc/backend/pkg/utils"
+)
+
+const (
+	shutdownTimeout   = 30 * time.Second
+	readHeaderTimeout = 5 * time.Second
 )
 
 func main() {
-	rb, err := router.NewRouteBuilder(slog.Default(), router.RouteBuilderOptions{TypesDirectory: "backend/internal/httpapi"})
+	config, err := app.NewConfig()
 	if err != nil {
-		panic(err)
+		fatalIfErr(slog.Default(), fmt.Errorf("failed to create config: %w", err))
 	}
+
+	defer func() {
+		if err := config.Close(); err != nil {
+			fatalIfErr(slog.Default(), fmt.Errorf("failed to close config: %w", err))
+		}
+	}()
+
+	logger := getLogger(config)
+
+	rb, err := router.NewRouteBuilder(logger, router.RouteBuilderOptions{TypesDirectory: "backend/internal/httpapi"})
+	fatalIfErr(logger, err)
+
 	rb.Get("/ping", router.RouteSpec{
 		OperationID: "ping",
 		Summary:     "Ping the server",
@@ -37,4 +63,63 @@ func main() {
 	})
 
 	rb.WriteSpecYAML("test.yaml")
+
+	addr := fmt.Sprintf(":%d", config.Port)
+	httpServer := &http.Server{
+		Addr:              addr,
+		Handler:           rb.Router(),
+		ReadHeaderTimeout: readHeaderTimeout,
+	}
+
+	sigCtx, sigCancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer sigCancel()
+
+	// Start HTTP/WS server
+	go func() {
+		logger.Info("http server listening", slog.String("address", addr))
+
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("server failed", utils.ErrAttr(err))
+			sigCancel()
+		}
+	}()
+
+	// Wait for signal (either OS or some failure)
+	<-sigCtx.Done()
+	logger.Info("received signal, shutting down...")
+
+	// Shutdown / Cleanup
+	logger.Info("http server shutting down...")
+
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer shutdownCancel()
+
+	if err := httpServer.Shutdown(shutdownCtx); err != nil {
+		logger.Error("http server shutdown failed", utils.ErrAttr(err))
+	}
+
+	logger.Info("http server shutdown complete")
+}
+
+func getLogger(config *app.Config) *slog.Logger {
+	logOptions := slog.HandlerOptions{
+		Level:       config.LogLevel,
+		ReplaceAttr: utils.SlogReplacer,
+	}
+
+	var logHandler slog.Handler = slog.NewJSONHandler(config.LogOutput, &logOptions)
+	if config.Generate {
+		logHandler = slog.NewTextHandler(config.LogOutput, &logOptions)
+	}
+
+	return slog.New(logHandler).With(slog.String("version", utils.GetVersionShort()))
+}
+
+func fatalIfErr(l *slog.Logger, err error) {
+	if err == nil {
+		return
+	}
+
+	l.Error("error", utils.ErrAttr(err))
+	os.Exit(1)
 }
